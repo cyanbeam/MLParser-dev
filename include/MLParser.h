c3857a539e0bda8f287accd84e6adaab22aa98a4
Cyan <a484599279@hotmail.com>
2018-12-20 17:17:35 +0800

    mmmmm

--- include/MLParser.h
+++ include/MLParser.h
@@ -10,6 +10,7 @@ namespace Cyan
 		char *raw;//saving raw text
 		Node *root;
 		Node *now;//saving a location for query
+		string errorMsg;
 		void Copy(MLParser & MLP)
 		{
 			if (MLP.raw == this->raw) return;
@@ -17,11 +18,12 @@ namespace Cyan
 
 			this->raw = Cyan::strcpy(MLP.raw);
 
-			root = MLP.root->Copy(nullptr);
-			now = root;
+			this->root = MLP.root->Copy(nullptr);
+			this->now = root;
+			this->errorMsg = nullptr;
 		}
 	public:
-		MLParser() :raw(nullptr), root(nullptr), now(nullptr) {}
+		MLParser() :raw(nullptr), root(nullptr), now(nullptr),errorMsg(nullptr) {}
 		MLParser(MLParser & MLP)
 		{
 			Copy(MLP);
@@ -40,6 +42,10 @@ namespace Cyan
 		string GetAttribute(const string & AttributeName) const;
 		string GetContent() const;//return content removed tags
 		string GetInner() const;//return raw inner text
+		string GetErrorMsg() const
+		{
+			return errorMsg;
+		}
 		void Dispose()
 		{
 			delete[] raw;
--- src/MLParser.cpp
+++ src/MLParser.cpp
@@ -1,5 +1,10 @@
 #include "MLParser.h"
 namespace Cyan
 {
-
+	bool MLParser::Parse(string html)
+	{
+		//TODO:Parser
+		errorMsg = "Not a standard HTML or XML format.";
+		return false;
+	}
 }
